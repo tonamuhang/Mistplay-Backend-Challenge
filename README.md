146f77f5793acee49a7ce33cd8b18bb620e1ee5c
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2020-03-15 15:29:25 -0400

    Update readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Mistplay-Backend-Challenge
 TODO List:
-1. Node.js setup
-2. Create database in MongoDB with AWS
-3. Integrate node js backend with React-native to create an android app.(Need to research a little)
-4. (If time permits)Possible update for better UI.
+1. Node.js setup - Done
+2. Create database in MongoDB with AWS - Done
+3. Integrate node js backend with React-native to create an android app.(Need to research a little) - Not done. Possibly
+to be updated in 48 hours.
+4. (If time permits)Possible update for better UI. - Not Done. Possibly to be updated in 48 hours.
+
+You can run the android app through the provided link:
+[Here](https://expo.io/@tonami/MistplayChallenge) by following
+the instructions given on the website.
--- frontend/MistplayChallenge/App.js
+++ frontend/MistplayChallenge/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import TextInput from "react-native-web/src/exports/TextInput";
+import { TextInput } from 'react-native';
 import validate from "react-native-web/dist/exports/StyleSheet/validate";
 
 export default function App() {
-  const [value, onChangeText] = React.useState('Search games');
-
   return (
     <View style={styles.container}>
       <Text>Please enter your search query: </Text>
       <TextInput
           style={{ height: 40, borderColor: 'gray', alignItems: 'center', borderWidth: 1 }}
-          onChangeText={text => onChangeText(text)}
-          value={value}
+          placeholder={'Search games'}
+          placeholderTextColor="#202020"
         />
     </View>
   );
