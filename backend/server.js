26d1fbf0ab4f4aec85a62084111cd27bfb85547d
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2020-03-15 12:27:44 -0400

    Create server.js with mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Create an express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Cors middleware to pass JSON
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+
+
+// Starts server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
