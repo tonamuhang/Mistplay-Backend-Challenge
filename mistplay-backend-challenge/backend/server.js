e91903744a2af20fe02fc249cd20d6e72cf2ce65
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2020-03-17 13:28:41 -0400

    .

--- mistplay-backend-challenge/backend/server.js
+++ mistplay-backend-challenge/backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 ).catch((err) => {
     console.error(err);
